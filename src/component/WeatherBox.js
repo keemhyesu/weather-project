8b39ad166c87f2b8b19a185d55584a046abc23c1
KIMHYESU <kimss9209@gmail.com>
2022-09-06 17:30:50 +0900

    Modify: temperature .toFixed(2) 메소드 추가

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -5,7 +5,7 @@ const WeatherBox = ({ weather }) => {
     <div className="outer">
       <h1>{weather?.name}</h1>
       <h2>
-        {weather?.main.temp}C / {weather?.main.temp * 1.8 + 32}F
+        {weather?.main.temp}C /{(weather?.main.temp * 1.8 + 32).toFixed(2)}F
       </h2>
       <h3>{weather?.weather[0].description}</h3>
     </div>
